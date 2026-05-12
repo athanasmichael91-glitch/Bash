72f9ae16fad42e0d464e8d4cd1445105831252bf
athanasmichael91-glitch <athanasmichael91@gmail.com>
2026-05-13 02:59:19 +0300

    Refactor app.js to include login function
    Removed HTML structure and added login function.

--- app.js
+++ app.js
@@ -1,16 +1,5 @@
-<body>
+function login() {
+  alert("Login button working");
+}
 
-  <div class="login-box">
-    <h1>POS LOGIN</h1>
-
-    <input type="email" id="email" placeholder="Email">
-
-    <input type="password" id="password" placeholder="Password">
-
-    <button onclick="login()">Login</button>
-  </div>
-
-  <script type="module" src="firebase.js"></script>
-  <script type="module" src="app.js"></script>
-
-</body>
+window.login = login;
