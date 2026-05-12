93918eb8e76a7f2a88871f7ca522eb52b17a6d1d
athanasmichael91-glitch <athanasmichael91@gmail.com>
2026-05-12 21:21:03 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,12 +1,10 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "APP_ID"
+  apiKey: "AIzaSyAcD6SHsAbex7xiWyBgM5_9X6AYH2Nb7Bs",
+  authDomain: "printhub-system.firebaseapp.com",
+  projectId: "printhub-system",
+  storageBucket: "printhub-system.firebasestorage.app",
+  messagingSenderId: "30090268076",
+  appId: "1:30090268076:web:82763f33a19f1500e6fe09",
+  measurementId: "G-PVVJHW59KG"
 };
-
-export const app = initializeApp(firebaseConfig);
