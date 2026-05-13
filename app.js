6e95dda1385c20b773eaa84153f3b137f5139f55
athanasmichael91-glitch <athanasmichael91@gmail.com>
2026-05-13 03:05:58 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
-function login() {
-  alert("Login button working");
-}
+import { auth } from "./firebase.js";
+import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-window.login = login;
+window.login = function () {
+
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  signInWithEmailAndPassword(auth, email, password)
+
+    .then(() => {
+      alert("Login Successful");
+      window.location.href = "dashboard.html";
+    })
+
+    .catch((error) => {
+      alert(error.message);
+    });
+
+}
